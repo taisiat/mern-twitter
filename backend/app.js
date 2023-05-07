6ebbea348d6104432146a14296175eca96c8c053
taisiat <113080884+taisiat@users.noreply.github.com>
2023-05-06 17:15:33 -0700

    Set up csrf protection

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
-// const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-// const indexRouter = require("./routes/index");
+const cors = require("cors");
+const csurf = require("csurf");
+const { isProduction } = require("./config/keys");
+
 const usersRouter = require("./routes/api/users");
 const tweetsRouter = require("./routes/api/tweets");
+const csrfRouter = require("./routes/api/csrf");
+const debug = require("debug");
 
 const app = express();
 
@@ -15,8 +19,49 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
 
+// ADD THIS SECURITY MIDDLEWARE
+// Security Middleware
+if (!isProduction) {
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, the Express
+  // server will serve the React files statically.)
+  app.use(cors());
+}
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
 // app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/tweets", tweetsRouter);
+app.use("/api/csrf", csrfRouter);
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug("backend:error");
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors,
+  });
+});
 
 module.exports = app;
